f54eaa3b13302179f94ceb1d92d1bcf2b4edb2ca
QueueElTee <tijaniqueen17@gmail.com>
2022-08-12 13:40:18 +0100

    Add Function To Evaluate Expressions In Pairs
    Also Refactored The Following Functions:
    1. operate
    2. clearDisplay

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const clear = document.querySelector('#clear');
 const backspace = document.querySelector('#backspace');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+let calcNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let operatorParameters = [];
+let operandParameters = [];
 
 
 const populateDisplay = () => {
@@ -12,8 +15,7 @@ const populateDisplay = () => {
     }));
 
     operators.forEach(operator => operator.addEventListener('click', () => {
-        let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        if(display.textContent != '' && numbers.includes(display.textContent.slice(-1))){
+        if(display.textContent != '' && calcNumbers.includes(display.textContent.slice(-1))){
             display.textContent += `${operator.textContent}`;
         };
     }));
@@ -23,6 +25,8 @@ const populateDisplay = () => {
 const clearDisplay = () => {
     clear.addEventListener('click', () => {
         display.textContent = '';
+        operatorParameters = [];
+        operandParameters = []
     });
 };
 
@@ -37,11 +41,6 @@ const clearCharacter = () => {
     });
 };
 
-populateDisplay();
-clearDisplay();
-clearCharacter();
-
-
 
 const add = (a, b) => a + b;
 
@@ -55,16 +54,50 @@ const div = (a, b) => a / b;
 const operate = (operator, num1, num2) => {
     switch(operator){
         case '+':
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case '-':
-            sub(num1, num2);
-            break;
+            return sub(num1, num2);
         case 'x':
-            mul(num1, num2);
-            break;
+            return mul(num1, num2);
         case '÷':
-            div(num1, num2);
-            break;
+            return div(num1, num2);
     }
 };
+
+
+// Evaluate Expressions In Pairs
+operators.forEach(operator => operator.addEventListener('click', () => {
+    if(display.textContent != '' && calcNumbers.includes(display.textContent.slice(-1))){
+        if(operatorParameters.length < 2){
+            operatorParameters.push(operator.textContent);
+            console.log(operatorParameters);
+        }
+
+        if(operatorParameters.length == 2){
+            operandParameters = display.textContent.split(/[÷|x|+|-]/);
+            console.log(operandParameters);
+            let num1 = parseInt(operandParameters[0]);
+            let num2 = parseInt(operandParameters[1]);
+            let currentOperator = operatorParameters[0];
+            let nextOperator = operatorParameters[1];
+
+            let exprDisplay = operate(currentOperator, num1, num2);
+            display.textContent = `${exprDisplay}${nextOperator}`;
+
+            operatorParameters.splice(0, operatorParameters.length)
+            operandParameters.splice(0, operandParameters.length)
+            operandParameters[0] = exprDisplay;
+            operatorParameters[0] = nextOperator;
+        }
+    }
+}));
+
+
+let testOperate = operate('-', 5, 7);
+console.log(testOperate);
+
+
+populateDisplay();
+clearDisplay();
+clearCharacter();
+// evaluateInPairs();
